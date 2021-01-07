f3d620d26e08c74de0eecd77796b5f04881a2847
Joshua Koudys <jkoudys@gmail.com>
2021-01-06 23:20:02 -0500

    feat: do a quick cleanout of some panics for Results

--- src/utils.rs
+++ src/utils.rs
@@ -1,6 +1,3 @@
-use serde_yaml;
-use std::collections::HashMap;
-use std::fs::File;
 use std::path::Path;
 
 pub fn clear_terminal() {
@@ -12,18 +9,4 @@ pub fn trim_file_ext(file_name: &str) -> String {
         Some(pretty_name) => String::from(pretty_name.to_str().unwrap()),
         None => String::from(file_name),
     }
-}
-
-pub fn parse_yaml_file(file_name: &str) -> HashMap<String, String> {
-    let f = match File::open(file_name) {
-        Err(msg) => panic!("Couldn't open {}: {}", file_name, msg),
-        Ok(file) => file,
-    };
-
-    let parsed_file: HashMap<String, String> = match serde_yaml::from_reader(f) {
-        Err(msg) => panic!("Error parsing YAML file!"),
-        Ok(parsed) => parsed,
-    };
-
-    parsed_file
 }
