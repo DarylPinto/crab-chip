ce1158b6d062dc80f0bac1d214cfa3c2361c592b
Joshua Koudys <jkoudys@gmail.com>
2021-01-06 23:59:53 -0500

    feat: add an error enum for core opcode errors

---
+++ src/core/error.rs
@@ -0,0 +1,18 @@
+use std::fmt;
+
+#[derive(Debug)]
+pub enum Error {
+    UnknownOpcode(u16),
+}
+
+impl std::error::Error for Error {}
+
+impl fmt::Display for Error {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Error::UnknownOpcode(op) => write!(f, "Unknown CHIP-8 opcode: {:#06x?}", op),
+        }
+    }
+}
+
+
